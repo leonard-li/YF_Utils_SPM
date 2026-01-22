7a17d55bbaa1726ee20f0afa61d73229a3c1061b
李强 <liqiang@game7.cc>
2026-01-22 15:11:48 +0800

    #1.2.6

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "YF_Utils",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "YF_Utils",
+            targets: ["YF_Utils"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YF_Utils",
+            url: "http://mvn.yifants.cn/artifactory/fineboost-spm/utils/1.2.6/YF_Utils.xcframework.zip",
+            checksum: "1576319bff229aa7ff806ff4c07c77986840a753ad860c44bf10b9828783ed3b"
+        )
+    ]
+)
