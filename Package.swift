d6ceeea60af0b124081d63c988003adbc542ddd2
李强 <liqiang@game7.cc>
2026-05-19 16:03:49 +0800

    #1.2.9

--- Package.swift
+++ Package.swift
@@ -11,13 +11,19 @@ let package = Package(
     products: [
         .library(
             name: "YF_Utils",
-            targets: ["YF_Utils"]
+            targets: ["YF_Utils_Target"]
         ),
     ],
     targets: [
+        .target(
+        name: "YF_Utils_Target",
+        dependencies: [
+            "YF_Utils_Binary"
+        ]
+        ),
         .binaryTarget(
             name: "YF_Utils",
-            url: "http://mvn.yifants.cn/artifactory/fineboost-spm/utils/1.2.9/YFUtils.xcframework.zip",
+            url: "https://mvn.yifants.cn/artifactory/fineboost-spm/utils/1.2.9/YFUtils.xcframework.zip",
             checksum: "71ed02a99a353f34d55d6e911e9d6010eeba696aa596d574aa482c87f9d33608"
         )
     ]
